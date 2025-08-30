50daa63bc7c911bf1ddf32aeb4b92ca755c56140
Mehmet Uysal <mehmetuysal2000@yandex.com>
2025-08-31 00:48:09 +0300

    update

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,6 @@
 import { UAuthPath, USessionPath } from "./classes/upath.mjs";
-import { UHost } from "./classes/uhost.mjs";
 import { UAuthService } from "./classes/uauth.mjs";
 import { USessionService } from "./classes/usession.mjs";
 
-export { UHost, UAuthPath, USessionPath };
+export { UAuthPath, USessionPath };
 export { UAuthService, USessionService };
